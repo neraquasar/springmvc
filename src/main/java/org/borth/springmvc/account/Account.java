7b7e6183a9b0c3c9654557ada1615bec10ffb693
kd <k.dichenko@borth.org>
2017-03-15 12:19:57 +0100

    account role added

--- src/main/java/org/borth/springmvc/account/Account.java
+++ src/main/java/org/borth/springmvc/account/Account.java
@@ -31,6 +31,9 @@ public class Account
     @Column
     private boolean active;
 
+    @Column
+    private Role role;
+
     public Long getId()
     {
         return id;
@@ -79,6 +82,16 @@ public class Account
     public void setActive(boolean active)
     {
         this.active = active;
+    }
+
+    public Role getRole()
+    {
+        return role;
+    }
+
+    public void setRole(Role role)
+    {
+        this.role = role;
     }
 
     @Override
---
+++ src/main/java/org/borth/springmvc/account/Role.java
@@ -0,0 +1,9 @@
+package org.borth.springmvc.account;
+
+/**
+ * Created by kd on 15.03.2017.
+ */
+public enum Role
+{
+    USER,ADMIN
+}
