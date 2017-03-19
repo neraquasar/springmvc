6277a19e5a2ad270f70c8788064c24c6c1b6bad0
Konstantin Dichenko <neraquasar@yandex.ru>
2017-03-19 16:04:31 +0100

    Wpring Security now works

--- src/main/java/org/borth/springmvc/config/AppConfig.java
+++ src/main/java/org/borth/springmvc/config/AppConfig.java
@@ -1,6 +1,7 @@
 package org.borth.springmvc.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,8 +18,6 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan("org.borth.springmvc")
-@PropertySource("classpath:config.properties")
 public class AppConfig extends WebMvcConfigurerAdapter
 {
 
--- src/main/java/org/borth/springmvc/config/DefaultWebAppInitializer.java
+++ src/main/java/org/borth/springmvc/config/DefaultWebAppInitializer.java
@@ -1,28 +1,48 @@
 package org.borth.springmvc.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Created by kd on 14.03.2017.
  */
-public class DefaultWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
-{
+@Configuration
+@ComponentScan("org.borth.springmvc")
+@PropertySource("classpath:config.properties")
+@Import(
+        {
+                AppConfig.class, PersistenceContext.class, WebSecurityConfig.class
+        })
+public class DefaultWebAppInitializer implements WebApplicationInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultWebAppInitializer.class);
 
     @Override
-    protected Class<?>[] getRootConfigClasses()
-    {
-        return new Class<?>[]{AppConfig.class, PersistenceContext.class, WebSecurityConfig.class};
-    }
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext webAppContext = new AnnotationConfigWebApplicationContext();
+        webAppContext.register(DefaultWebAppInitializer.class);
+        servletContext.addListener(new ContextLoaderListener(webAppContext));
 
-    @Override
-    protected Class<?>[] getServletConfigClasses()
-    {
-        return new Class<?>[]{AppConfig.class};
-    }
+        FilterRegistration.Dynamic security = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
+        security.addMappingForUrlPatterns(null, false, "/*");
 
-    @Override
-    protected String[] getServletMappings()
-    {
-        return new String[]{"/"};
+        webAppContext.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("spring-mvc", new DispatcherServlet(webAppContext));
+        servlet.addMapping("/*");
+        servlet.setLoadOnStartup(1);
     }
 }
